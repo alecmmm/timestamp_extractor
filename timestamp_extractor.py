a61de3f15ed7f3f35b5256d390b7a5b38fdf6b01
Alec McKay <alecmckay90@gmail.com>
2020-06-30 19:03:18 -0400

    Update timestamp_extractor.py

--- timestamp_extractor.py
+++ timestamp_extractor.py
@@ -11,13 +11,9 @@ See: https://stackoverflow.com/questions/237079/how-to-get-file-creation-modific
 def creation_date(path_to_file):
 
     """
-
     Try to get the date that a file was created, falling back to when it was
-
     last modified if that isn't possible.
-
     See http://stackoverflow.com/a/39501288/1709587 for explanation.
-
     """
 
     if platform.system() == 'Windows':
